9b192a8b1cec499e77ca65bfe95ffcf6a9b7ced5
Charlie Hield <charles.hield@gmail.com>
2026-02-10 02:37:58 -0600

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
-  integrations: [tailwind()],
-  experimental: {
-    session: {
-      driver: 'cookie'
-    }
-  }
+  integrations: [tailwind()]
 })
